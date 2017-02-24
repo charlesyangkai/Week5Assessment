0fa86546b5373adffb61121e329f955ef5af6682
Charles Lee <charleslee94@hotmail.com>
2017-02-24 13:16:23 +0800

    first commit

---
+++ Week5Assessment/BikeStation.swift
@@ -0,0 +1,29 @@
+//
+//  BikeStation.swift
+//  Week5Assessment
+//
+//  Created by Charles Lee on 24/2/17.
+//  Copyright © 2017 NextAcademy. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class BikeStation {
+    
+    static var allBikeStations : [BikeStation] = []
+    var stationName: String
+    var availableBikes: Int
+    var latitude: Double
+    var longitude: Double
+    //var distance: String
+    
+    
+    
+    init(json: JSON){
+        stationName = json["stationName"].stringValue
+        availableBikes = json["availableBikes"].intValue
+        latitude = json["latitude"].doubleValue
+        longitude = json["longitude"].doubleValue
+    }
+}
--- Week5Assessment/ViewController.swift
+++ Week5Assessment/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    
+    
+    let locationManager = CLLocationManager()
+    var resultSearchController: UISearchController? = nil
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        // Search Bar & Table View Controller
+        let tableViewController = storyboard?.instantiateViewController(withIdentifier: "TableViewController") as! TableViewController
+        resultSearchController = UISearchController(searchResultsController: tableViewController)
+        resultSearchController?.searchResultsUpdater = tableViewController
+        
+        let searchBar = resultSearchController?.searchBar
+        searchBar?.sizeToFit()
+        searchBar?.placeholder = "Search for bike stations"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
 
+
+
+extension ViewController: CLLocationManagerDelegate{
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Sian la got this stoopid twat error: \(error)")
+    }
+    
+    // Info will be updated so we can get the latest info
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+        
+        // Span to current location
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
